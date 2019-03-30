018de68de933ebe70df5c1f51c132b67f5aa2f3e
Zaphyk <zaphyk@live.com>
2019-03-30 01:02:11 -0300

    mejorar algunas cosas para atenerse a las reglas de estilo

--- caliz.c
+++ caliz.c
@@ -1,32 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-#define CAPACIDAD_MAGICA_REGULAR "R"
-#define CAPACIDAD_MAGICA_BUENA "B"
-#define CAPACIDAD_MAGICA_MALA "M"
+#define CAPACIDAD_MAGICA_REGULAR 'R'
+#define CAPACIDAD_MAGICA_BUENA 'B'
+#define CAPACIDAD_MAGICA_MALA 'M'
 #define MAX_INT 2147483647
 
-void recibir_numero(int* valor, int min, int max) {
+void recibir_numero(int* valor, int min, int max, char* mensaje, char* mensaje_de_error) {
+    printf(mensaje);
+    bool es_invalido = false;
     do {
         scanf("%i", valor);
-        if (valor >= min && valor <= max) break;
-        printf("Dato invalido. El valor a ingresar debe ser mayor que %d y menor que %d\n", min, max);
+        if (es_invalido = ((*valor) < min || (*valor) > max)) 
+            printf(mensaje_de_error, min, max);
     }
-    while(true);
+    while(es_invalido);
 }
 
-void recibir_inteligencia(int* inteligencia) {
-    printf("Ingresa la inteligencia del estudiante:\n");
-    recibir_numero(inteligencia, 0, 10);
-}
-
-void recibir_fuerza(int* fuerza) {
-    printf("Ingresa la fuerza del estudiante:\n");
-    recibir_numero(fuerza, 0, 10);
-}
-
-void recibir_edad(int* edad) {
-    printf("Ingresa la edad del estudiante:\n");
-    recibir_numero(edad, 17, MAX_INT);
+void recibir_atributo(int* valor, int min, int max, char* mensaje) {
+    recibir_numero(valor, min, max, mensaje, "Dato invalido. El valor a ingresar debe ser mayor que %d y menor que %d\n");
 }
 
 void parsear_capacidad_magica(char raw, int* capacidad_magica) {
@@ -44,43 +36,47 @@ void parsear_capacidad_magica(char raw, int* capacidad_magica) {
 }
 
 void recibir_capacidad_magica(int* capacidad_magica) {
+    printf("Ingrese la capacidad magica del estudiante\n");
     char raw_capacidad_magica;
+    bool es_invalido = false;
     do {
-        scanf("%c", &raw_capacidad_magica);
-        if (raw_capacidad_magica == CAPACIDAD_MAGICA_MALA || raw_capacidad_magica == CAPACIDAD_MAGICA_REGULAR || raw_capacidad_magica == CAPACIDAD_MAGICA_BUENA) break;
-        printf("Dato invalido. El valor a ingresar debe ser alguna de estas opciones: '%c', '%c' o '%c'\n", CAPACIDAD_MAGICA_MALA, CAPACIDAD_MAGICA_REGULAR, CAPACIDAD_MAGICA_BUENA);
+        scanf(" %c", &raw_capacidad_magica);
+        es_invalido = !(raw_capacidad_magica == CAPACIDAD_MAGICA_MALA || raw_capacidad_magica == CAPACIDAD_MAGICA_REGULAR || raw_capacidad_magica == CAPACIDAD_MAGICA_BUENA); 
+        if (es_invalido)
+            printf("Dato invalido. El valor a ingresar debe ser alguna de estas opciones: '%c', '%c' o '%c'\n", CAPACIDAD_MAGICA_MALA, CAPACIDAD_MAGICA_REGULAR, CAPACIDAD_MAGICA_BUENA);
     }
-    while(true);
+    while(es_invalido);
     parsear_capacidad_magica(raw_capacidad_magica, capacidad_magica);
 }
 
-bool recibir_estudiante(int* edad, int* inteligencia, int* fuerza, int* capacidad_magica) {
-    recibir_edad(edad);
-    recibir_inteligencia(inteligencia);
-    recibir_fuerza(fuerza);
+void recibir_estudiante(int* edad, int* inteligencia, int* fuerza, int* capacidad_magica) {
+    recibir_numero(edad, 17, MAX_INT, "Ingresa la edad del estudiante:\n", "Dato invalido. El valor a ingresar debe ser mayor que %d\n");
+    recibir_atributo(inteligencia, 0, 10, "Ingresa la inteligencia del estudiante:\n");
+    recibir_atributo(fuerza, 0, 10, "Ingresa la fuerza del estudiante:\n");
     recibir_capacidad_magica(capacidad_magica);
-    return true;
 }
 
-void hacer_campeon(int* campeon_id, int* campeon_inteligencia, int* campeon_fuerza, int* campeon_capacidad_magica, int id, int inteligencia, int fuerza, int capacidad_magica) {
+bool hacer_campeon(int* campeon_id, int* campeon_inteligencia, int* campeon_fuerza, int* campeon_capacidad_magica, int id, int inteligencia, int fuerza, int capacidad_magica) {
     (*campeon_id) = id;
     (*campeon_inteligencia) = inteligencia;
     (*campeon_fuerza) = fuerza;
     (*campeon_capacidad_magica) = capacidad_magica;
+    return true;
+}
+
+bool comparar_atributo(int* campeon_id, int* campeon_inteligencia, int* campeon_fuerza, int* campeon_capacidad_magica, int id, int inteligencia, int fuerza, int capacidad_magica, int campeon_atributo, int atributo) {
+
+    if (campeon_atributo < atributo) return hacer_campeon(campeon_id, campeon_inteligencia, campeon_fuerza, campeon_capacidad_magica, id, inteligencia, fuerza, capacidad_magica);
+    else if(campeon_atributo > atributo) return true;
+    return false;
 }
 
 void comparar_estudiantes(int* campeon_id, int* campeon_inteligencia, int* campeon_fuerza, int* campeon_capacidad_magica, int id, int edad, int inteligencia, int fuerza, int capacidad_magica) {
 
     if (edad < 18) return;
-
-    if((*campeon_inteligencia) < inteligencia) hacer_campeon(campeon_id, campeon_inteligencia, campeon_fuerza, campeon_capacidad_magica, id, inteligencia, fuerza, capacidad_magica);
-    else if ((*campeon_inteligencia) > inteligencia) return;
-
-    if((*campeon_fuerza) < fuerza)hacer_campeon(campeon_id, campeon_inteligencia, campeon_fuerza, campeon_capacidad_magica, id, inteligencia, fuerza, capacidad_magica);
-    else if ((*campeon_fuerza) > fuerza) return;
-
-    if((*campeon_capacidad_magica) < capacidad_magica) hacer_campeon(campeon_id, campeon_inteligencia, campeon_fuerza, campeon_capacidad_magica, id, inteligencia, fuerza, capacidad_magica);
-    else if ((*campeon_capacidad_magica) > capacidad_magica) return;
+    if (comparar_atributo(campeon_id, campeon_inteligencia, campeon_fuerza, campeon_capacidad_magica, id, inteligencia, fuerza, capacidad_magica, (*campeon_inteligencia), inteligencia)) return;
+    if (comparar_atributo(campeon_id, campeon_inteligencia, campeon_fuerza, campeon_capacidad_magica, id, inteligencia, fuerza, capacidad_magica, (*campeon_fuerza), fuerza)) return;
+    if (comparar_atributo(campeon_id, campeon_inteligencia, campeon_fuerza, campeon_capacidad_magica, id, inteligencia, fuerza, capacidad_magica, (*campeon_capacidad_magica), capacidad_magica)) return;
 }
 
 void mostrar_campeon(int id) {
@@ -90,23 +86,25 @@ void mostrar_campeon(int id) {
         printf("No hay campeón :(\n");
 }
 
-bool no_desea_ingresar_mas() {
+void preguntar_si_seguir_ingresando(bool* resultado) {
     char respuesta;
-    printf("Desea ingresar otro alumno? (N para ​NO​, cualquier otra tecla para sí): S\n");
-    scanf("%c", &respuesta);
-    return respuesta == "N";
+    printf("Desea ingresar otro alumno? (N para ​NO​, cualquier otra tecla para sí)\n");
+    scanf(" %c", &respuesta);
+    (*resultado) = respuesta != 'N';
 }
 
 int main() {
     int campeon_id = 0, campeon_inteligencia, campeon_fuerza, campeon_capacidad_magica;
     int id_actual = 1, edad, inteligencia, fuerza, capacidad_magica;
 
-    while(recibir_estudiante(&edad, &inteligencia, &fuerza, &capacidad_magica)) {
+    bool esta_ingresando = true;
+    while(esta_ingresando) {
+        recibir_estudiante(&edad, &inteligencia, &fuerza, &capacidad_magica);
         comparar_estudiantes(
             &campeon_id, &campeon_inteligencia, &campeon_fuerza, &campeon_capacidad_magica,
             id_actual++, edad, inteligencia, fuerza, capacidad_magica
         );
-        if (no_desea_ingresar_mas()) break;
+        preguntar_si_seguir_ingresando(&esta_ingresando);
     }
     mostrar_campeon(campeon_id);
     return 0;
